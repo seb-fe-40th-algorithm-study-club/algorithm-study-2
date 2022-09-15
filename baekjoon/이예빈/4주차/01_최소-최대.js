3ac4186ef0e77e8528d64685695171fc419b0d87
2pandi <coder.2pandi@gmail.com>
2022-09-16 00:00:57 +0900

    Revert "이예빈 1~4주차"

--- baekjoon/이예빈/4주차/01_최소-최대.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n").map(v => v.split(' ').map(v => +v));
-
-let [N, arr] = input;
-let min = Math.min(...arr);
-let max = Math.max(...arr);
-
-console.log(`${min} ${max}`);
