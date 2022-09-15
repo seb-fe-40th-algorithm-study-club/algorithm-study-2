f45d2bd89a61d0df695732ce5af12c713ee86ca0
2pandi <coder.2pandi@gmail.com>
2022-09-16 00:00:57 +0900

    Revert "이예빈 1~4주차"

--- baekjoon/이예빈/1주차/01_Hello-Wrold.js
+++
@@ -1,6 +0,0 @@
-//한 줄인 경우
-let fs = require('fs');
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split(" ");
-
-console.log("Hello World!");
